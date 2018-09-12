1f56293851228b8c98c85caab7acd9f1bea21dec
yashwanth033 <31200466+yashwanth033@users.noreply.github.com>
2018-09-12 17:23:57 +0530

    added coverage html report for jenkins

--- JenkinsBuild.bat
+++ JenkinsBuild.bat
@@ -3,4 +3,5 @@ python hello.py %
 pytest hello.py --junitxml=test_reports/test_report.xml %
 python -m coverage run hello.py%
 python -m coverage xml -o coverage.xml %
+python -m coverage html -d coverage %
 pylint hello.py --max-line-length=120 pystache > pylint.log || true %
