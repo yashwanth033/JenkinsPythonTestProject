84bc582e32132e39976be7a84dd7eecd4c6d6115
yashwanth033 <31200466+yashwanth033@users.noreply.github.com>
2018-09-12 16:39:10 +0530

    Changed bat file to add coverage config.

--- JenkinsBuild.bat
+++ JenkinsBuild.bat
@@ -1,4 +1,5 @@
 SET PATH=%PATH%;%PYTHON_PATH%
 python hello.py %
 pytest hello.py --junitxml=test_reports/test_report.xml %
+python -m coverage xml -o coverage.xml %
 pylint hello.py --max-line-length=120 pystache > pylint.log || true %
