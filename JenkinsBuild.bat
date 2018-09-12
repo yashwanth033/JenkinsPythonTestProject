0c4dc4d706ce7a285082e4f59e10db5c7d508102
yashwanth033 <31200466+yashwanth033@users.noreply.github.com>
2018-09-12 16:51:26 +0530

    added modifications for testing coverage in Jenkins.

--- JenkinsBuild.bat
+++ JenkinsBuild.bat
@@ -1,5 +1,6 @@
 SET PATH=%PATH%;%PYTHON_PATH%
 python hello.py %
 pytest hello.py --junitxml=test_reports/test_report.xml %
-pylint hello.py --max-line-length=120 pystache > pylint.log || true %
+python -m coverage run hello.py%
 python -m coverage xml -o coverage.xml %
+pylint hello.py --max-line-length=120 pystache > pylint.log || true %
