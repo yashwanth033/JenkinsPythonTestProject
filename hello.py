8a2ab8283d81911a696dd077213fdf886ac4d118
yashwanth033 <31200466+yashwanth033@users.noreply.github.com>
2018-09-12 10:09:29 +0530

    Revert back to make pytest work.

--- hello.py
+++ hello.py
@@ -1,12 +1,6 @@
-import xmlrunner
-from openpyxl.styles.builtins import output
-
 def inc(x):
     return x + 1
 
 
 def test_answer():
     assert inc(4) == 5
-
-testRunner = xmlrunner.XMLTestRunner(output="test_reports")
-testRunner.run(test_answer())
