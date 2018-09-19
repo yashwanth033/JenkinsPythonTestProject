48985d8cbf14704c5836cfa1a48d4f0a22f69c4a
yashwanth033 <31200466+yashwanth033@users.noreply.github.com>
2018-09-19 20:09:58 +0530

    Update JenkinsBuild.bat

--- JenkinsBuild.bat
+++ JenkinsBuild.bat
@@ -4,4 +4,4 @@ pytest hello.py --junitxml=test_reports/test_report.xml %
 python -m coverage run hello.py%
 python -m coverage xml -o coverage.xml %
 python -m coverage html -d coverage %
-pylint hello.py --max-line-length=120 ./ > pylint.log || true %
+pylint hello.py > pylint.log || true %
