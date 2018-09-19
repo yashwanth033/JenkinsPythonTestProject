ee908aadc1d2f0e25e2eda380de47238a7a4a0d9
yashwanth033 <31200466+yashwanth033@users.noreply.github.com>
2018-09-19 19:43:16 +0530

    Update JenkinsBuild.sh

--- JenkinsBuild.sh
+++ JenkinsBuild.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+
+. ~/usr/bin/python3/bin/activate
+
 set -e
-python3 hello.py
+set -x
+
+rm -f pylint.log
+
+PYTHONPATH=.
+
 pytest hello.py --junitxml=test_reports/test_report.xml
 python3 -m coverage run hello.py
 python3 -m coverage xml -o coverage.xml
