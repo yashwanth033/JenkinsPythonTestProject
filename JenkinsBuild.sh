7c4ae7dd332fdff65437f380d76b150825db675d
yashwanth033 <31200466+yashwanth033@users.noreply.github.com>
2018-09-19 18:34:55 +0530

    Added bash file for Linux Jenkins

---
+++ JenkinsBuild.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+python3 hello.py
+pytest hello.py --junitxml=test_reports/test_report.xml
+python3 -m coverage run hello.py
+python3 -m coverage xml -o coverage.xml
+python3 -m coverage html -d coverage
+pylint hello.py --max-line-length=120 pystache > pylint.log || true
