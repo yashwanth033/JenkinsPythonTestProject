4527f44741f2570887518bbb0a42a935c4f20366
yashwanth033 <31200466+yashwanth033@users.noreply.github.com>
2018-09-19 20:44:47 +0530

    Update JenkinsBuild.bat

--- JenkinsBuild.bat
+++ JenkinsBuild.bat
@@ -4,4 +4,4 @@ pytest hello.py --junitxml=test_reports/test_report.xml %
 python -m coverage run hello.py%
 python -m coverage xml -o coverage.xml %
 python -m coverage html -d coverage %
-pylint hello.py > pylint.log || true %
+pylint --rcfile=pylint.cfg $(find . -maxdepth 1 -name "*.py" -print) MYMODULE/ > pylint.log || exit 0 %
